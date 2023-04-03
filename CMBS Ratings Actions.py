d73ea901c12f415b58fb266e7a52bb5d5d8d01f5
James Clark <97779921+jamesclark1314@users.noreply.github.com>
2023-04-03 10:54:52 -0400

    Add files via upload

--- CMBS Ratings Actions.py
+++ CMBS Ratings Actions.py
@@ -7,10 +7,13 @@ Created on Tue Mar 21 08:50:45 2023
 
 import pandas as pd
 
-bofa_file = pd.read_excel('Ratings Actions 3-24-23.xlsx', 
-                          sheet_name = 'Week ending 3.24.2023')
+bofa_file = pd.read_excel('Ratings Actions 4-03-23.xlsx', 
+                          sheet_name = 'Week ending 3.31.2023')
 
 holdings = pd.read_excel('BBG Alert Template.xlsx', sheet_name = 'Holdings')
+
+# Maintain a copy of the original file
+bofa_file_orig = bofa_file
 
 # Set column headers and drop unneeded columns/rows
 bofa_file.columns = bofa_file.iloc[0]
@@ -20,8 +23,14 @@ del bofa_file['CMBX']
 holdings.columns = holdings.iloc[3]
 holdings = holdings.iloc[4:,1:-1]
 
-# Drop affirmed 
+# Drop affirmed, revised outlook
 bofa_file = bofa_file[bofa_file['Action'].str.contains('Affirmed') == False]
+bofa_file = bofa_file[bofa_file['Action'].str.contains(
+    'Revised Outlook - Negative') == False]
+bofa_file = bofa_file[bofa_file['Action'].str.contains(
+    'Revised Outlook - Positive') == False]
+bofa_file = bofa_file[bofa_file['Action'].str.contains(
+    'Revised Outlook - Stable') == False]
 
 # Create a list of cusips that show up in both files
 cusip_match = pd.merge(bofa_file, holdings, on = ['Cusip'])
@@ -39,6 +48,10 @@ del deal_match['Cusip_y']
 del deal_match['Class']
 del deal_match['Date']
 
+# Drop duplicates
+deal_match = deal_match.drop_duplicates()
+
 # Only include upgrades and downgrades
 deal_match = deal_match.loc[deal_match['Action'].str.contains(
     'Downgraded|Upgraded')]
+
